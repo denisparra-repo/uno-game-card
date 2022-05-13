2c86c9d0116ded20db2f78545692e7c1df184bce
Denis Parra <denis.parra.89@outlook.com>
2022-05-12 21:21:26 -0400

    new resources to get cards addded

--- uno-card-game-server/server.js
+++ uno-card-game-server/server.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+const {getCards} = require("./card.utils")
+let cards = getCards();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/init-cards', (req, res) => {
+  let result = [];
+  result.push(cards())
+  result.push(cards())
+  result.push(cards())
+  result.push(cards())
+  result.push(cards())
+  res.send(result)
+})
+
+app.get('/new-card', (req, res) => {
+  res.send(cards())
 })
 
 app.listen(port, () => {
